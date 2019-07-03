6718d2e99c7cbec31bb52995337236f17c1bf5ec
Arthur Sun <arthursun2005@gmail.com>
2019-07-03 12:08:08 +1000

    Initial Commit

---
+++ 24/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  24
+//
+//  Created by Arthur Sun on 7/3/19.
+//  Copyright © 2019 Arthur Sun. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
